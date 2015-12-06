e6857c128ea7b7405f59043e93a33238fd4f580d
shawnv98 <shawn.verma3@gmail.com>
2015-12-06 17:21:20 -0500

    Updated statistics

--- CoffeeShopSimulation/CoffeeShopSimulation/CoffeeShopSimulation/Statistics.cs
+++ CoffeeShopSimulation/CoffeeShopSimulation/CoffeeShopSimulation/Statistics.cs
@@ -4,9 +4,30 @@
 // Date Modified: Dec 5th 2015
 // Description: 
 
+using Microsoft.Xna.Framework.Graphics;
 namespace CoffeeShopSimulation
 {
     class Statistics
     {
+        private float minWaitTime;
+        private float maxWaitTime;
+        private float avgWaitTime;
+        private float visits;
+        private float[] longestWaitTimes = new float [4];
+
+        public void Update(Queue<Customer> customers)
+        {
+            Node<Customer> curCustomer = customers.Peek();
+
+            for(int i = 0; i < customers.Size; i++)
+            {
+
+            }
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+
+        }
     }
 }
