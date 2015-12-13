37cfb285bbe7a853f7d75257ff286dcf1eab653b
DevHalo <devhalo@hotmail.com>
2015-12-12 22:41:00 -0500

    Draw background and cashiers

--- CoffeeShopSimulation/CoffeeShopSimulation/CoffeeShopSimulation/SimulationView.cs
+++ CoffeeShopSimulation/CoffeeShopSimulation/CoffeeShopSimulation/SimulationView.cs
@@ -15,12 +15,11 @@ namespace CoffeeShopSimulation
         // Assets
         private Texture2D customerTexture;
         private Texture2D cashierTexture;
-        private Texture2D lineTexture;
-        private Texture2D floorTexture;
-        private Texture2D wallTexture;
+        private Texture2D backgroundTexture;
         private Texture2D pixelTexture;
         private Texture2D counterTexture;
         private SpriteFont mainFont;
+        private SpriteFont smallFont;
 
         /// <summary>
         /// Initializes the view and loads all assets into memory
@@ -34,17 +33,22 @@ namespace CoffeeShopSimulation
             pixelTexture.SetData(new [] {Color.White});
 
             // Load the rest of the textures
+            backgroundTexture = content.Load<Texture2D>("Images/background");
 
             // Load fonts
             mainFont = content.Load<SpriteFont>("Fonts/bigFont");
+            smallFont = content.Load<SpriteFont>("Fonts/smallFont");
         }
 
         public void Draw(SpriteBatch sb, SimulationModel model)
         {
+            sb.Draw(backgroundTexture, Vector2.Zero, Color.White);
+
             // Draw statistics and other important information
-            sb.DrawString(mainFont, "Tim Hortons Simulator 2015", Vector2.Zero, Color.Red);
-            sb.DrawString(mainFont, "Simulation Time: " + model.SimTime + " Seconds", new Vector2(0, 25), Color.Red);
-            sb.DrawString(mainFont, "Number of Customers: " + model.CustomersInStore, new Vector2(0, 50), Color.Red);
+            sb.Draw(pixelTexture, new Rectangle(0, 0, 420, 100), Color.White * 0.8f);
+            sb.DrawString(mainFont, "Tim Hortons Simulator 2015", Vector2.Zero, Color.Blue);
+            sb.DrawString(mainFont, "Simulation Time: " + model.SimTime + " Seconds", new Vector2(0, 25), Color.Blue);
+            sb.DrawString(mainFont, "Number of Customers: " + model.CustomersInStore, new Vector2(0, 50), Color.Blue);
 
             if (model.Paused)
             {
@@ -56,9 +60,14 @@ namespace CoffeeShopSimulation
             for (int i = 0; i < model.Customers.Size; i++)
             {
                 // Draw the customer stored in the node
-                curNode.Value.View.Draw(sb, pixelTexture, mainFont);
+                curNode.Value.View.Draw(sb, pixelTexture, smallFont);
                 // Iterate to the next node
                 curNode = curNode.GetNext();
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                sb.Draw(pixelTexture, new Rectangle(1300, 200 + (100 * i), 20, 20), Color.Brown);
             }
         }
     }
