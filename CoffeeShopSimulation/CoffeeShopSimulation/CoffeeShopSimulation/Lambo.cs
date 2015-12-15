ac83a4eca19418f61dd9663fa5671fa1cbbcda33
SanjayP9 <paraboosanjay@gmail.com>
2015-12-14 21:29:10 -0500

    Updated Lambo.cs

--- CoffeeShopSimulation/CoffeeShopSimulation/CoffeeShopSimulation/Lambo.cs
+++ CoffeeShopSimulation/CoffeeShopSimulation/CoffeeShopSimulation/Lambo.cs
@@ -8,10 +8,47 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace CoffeeShopSimulation
 {
     class Lambo
     {
+
+        private Texture2D lamboImg;
+
+        private Vector2 lamboLocal;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Lambo(Texture2D lamboImg)
+        {
+            this.lamboImg = lamboImg;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public void Update(float gameTime)
+        {
+
+        }
+
+        public void Spawn()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sb"></param>
+        public void Draw(SpriteBatch sb)
+        {
+            sb.Draw(lamboImg, lamboLocal, null, Color.White, 0f, Vector2.Zero, 1.0f, SpriteEffects.None, 0);
+        }
     }
 }
