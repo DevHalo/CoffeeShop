74c715f442a75d8ccf24d29eca21d4c7581ea3a6
SanjayP9 <paraboosanjay@gmail.com>
2015-12-14 21:23:18 -0500

    added lambo header

--- CoffeeShopSimulation/CoffeeShopSimulation/CoffeeShopSimulation/Lambo.cs
+++ CoffeeShopSimulation/CoffeeShopSimulation/CoffeeShopSimulation/Lambo.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿// Author: Sanjay Paraboo, Mark Vuoong, Shawn Verma
+// File Name: CustomerView.cs
+// Project Name: A5 Data Manipulation Assignment
+// Creation Date: Dec 5th, 2015
+// Modified Date: Dec 14th, 2015
+// Description:
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
